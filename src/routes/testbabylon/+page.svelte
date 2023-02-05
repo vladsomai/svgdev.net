44d931eb360b7c55c5d127c1894a80bb71ba0cf9
Vlad Somai <Vlad.Somai-3rdpty@brooks.com>
2023-02-05 13:03:46 +0200

    fix 3d page

--- src/routes/testbabylon/+page.svelte
+++ src/routes/testbabylon/+page.svelte
@@ -1,45 +1,87 @@
 <script lang="ts">
 	import * as BABYLON from 'babylonjs';
-	import { onMount, onDestroy } from 'svelte';
+	import 'babylonjs-loaders';
+	import { onMount } from 'svelte';
 
 	let container3d: HTMLCanvasElement;
+	let scene: BABYLON.Scene | null = null;
+	let camera: BABYLON.FreeCamera | null = null;
+	let dragonAnimations = '';
+
 	class Playground {
 		public static CreateScene(canvas: HTMLCanvasElement): BABYLON.Scene {
 			const engine = new BABYLON.Engine(canvas, true);
-			let scene = new BABYLON.Scene(engine);
-			var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
-			camera.setTarget(BABYLON.Vector3.Zero());
+			scene = new BABYLON.Scene(engine);
+			camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
 			camera.attachControl(canvas, true);
-			var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+			let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 			light.intensity = 0.7;
-			// var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2, segments: 32 }, scene);
-			// sphere.position.y = 1;
-			// var ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 10, height: 10 }, scene);
 
-			// BABYLON.SceneLoader.Load('../../src/lib/3d-assets/', 'Dragon.glb', engine, function (scene) {
-			// 	console.log('ok');
-			// 	// scene.createDefaultCamera(true, true, true);
-			// });
 			BABYLON.SceneLoader.Append('../../src/lib/3d-assets/', 'Dragon.glb', scene, function (scene) {
-				// Create a default arc rotate camera and light.
+				// // Create a default arc rotate camera and light.
 				scene.createDefaultCameraOrLight(true, true, true);
+				//@ts-ignore
+				scene.activeCamera!.alpha += -Math.PI / 1.2;
 
-				// The default camera looks at the back of the asset.
-				// Rotate the camera by 180 degrees to the front of the asset.
-				scene!.activeCamera!.alpha += Math.PI;
+				//@ts-ignore
+				scene.activeCamera!.radius += -0.8;
 			});
 
 			engine.runRenderLoop(function () {
-				scene.render();
+				scene?.render();
 			});
 
 			return scene;
 		}
 	}
 
+	function changeAnimation() {
+		if (scene == null) return;
+
+		switch (dragonAnimations[0]) {
+			case 'flying':
+				const flying = scene.getAnimationGroupByName('flying');
+				flying?.start(true, 1, flying.from, flying.to, false);
+				//@ts-ignore
+				scene.activeCamera!.alpha = 1.2;
+
+				//@ts-ignore
+				scene.activeCamera!.beta = 1.4;
+
+				//@ts-ignore
+				scene.activeCamera!.radius = 1;
+				break;
+			case 'idle':
+				const idle = scene.getAnimationGroupByName('idle');
+				break;
+			case 'running':
+				const running = scene.getAnimationGroupByName('running');
+				break;
+		}
+	}
+
+	$: {
+		console.log(dragonAnimations[0]);
+
+		changeAnimation();
+	}
 	onMount(() => {
 		Playground.CreateScene(container3d);
 	});
 </script>
 
-<canvas bind:this={container3d} class="w-screen h-screen fixed" />
+<svelte:head>
+	<title>3D Dragon</title>
+	<meta name="description" content="display 3d dragon" />
+</svelte:head>
+
+<div class="w-screen h-screen relative">
+	<canvas bind:this={container3d} class="w-full h-full absolute" />
+
+	<!-- <select class="fixed" multiple bind:value={dragonAnimations}>
+		<option value="flying">flying</option>
+		<option value="idle">idle</option>
+		<option value="running">running</option>
+	</select> -->
+</div>
